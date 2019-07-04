59a036f74042585e74d23c043a4e55f18ef6c27a
jatin510 <jatin6972@gmail.com>
2019-07-04 12:57:52 +0530

    have to change the add kitchen manager and change the name of the kitchen collection to kitchens

--- routes/api/users/trial.js
+++ routes/api/users/trial.js
@@ -4,25 +4,392 @@ const db = admin.firestore();
 const route = require("express").Router();
 const Joi = require("@hapi/joi");
 
+async function getSubscriptions(value){
+  let subSchema = {}
+
+  if(value.breakfast){
+    subSchema.breakfast = {}
+    subSchema.breakfast.address = {}
+    subSchema.breakfast.address.coordinates = {}
+    subSchema.breakfast.status = {}
+
+    /// user sector /////
+
+    let kitchenManagerDocRef = await db.collection('kitchens').where(`areaHandling.${userSector}`,"==",true).get()
+
+    kitchenManagerDocRef.forEach(doc =>{
+      console.log('Kitchen breakfast getting sector',doc.data())
+      let id = doc.id
+      let kitchenName = doc.data().name
+
+      subSchema.breakfast.kitchen = {}
+      subSchema.breakfast.kitchen.id = id
+      subSchema.breakfast.kitchen.name = kitchenName
+
+    })
+
+    if (value.breakfast.lite) subSchema.breakfast.lite = true;
+    if (value.breakfast.full) subSchema.breakfast.full = true;
+    subSchema.breakfast.address.id = value.breakfast.address.id;
+    subSchema.breakfast.address.tag = value.breakfast.address.tag;
+    subSchema.breakfast.address.coordinates.latitude =
+      value.breakfast.address.coordinates.latitude;
+    subSchema.breakfast.address.coordinates.longitude =
+      value.breakfast.address.coordinates.longitude;
+    subSchema.breakfast.address.address1 = value.breakfast.address.address1;
+    subSchema.breakfast.address.address2 = value.breakfast.address.address2;
+    subSchema.breakfast.address.area = value.breakfast.address.area;
+    subSchema.breakfast.address.city = value.breakfast.address.city;
+    subSchema.breakfast.price = value.breakfast.price;
+    subSchema.breakfast.quantity = value.breakfast.quantity;
+    if (subSchema.breakfast.quantity !== 0) {
+      subSchema.breakfast.status.upcoming = true;
+    } else {
+      subSchema.breakfast.status.upcoming = false;
+    }
+  }
+
+  if (value.lunch) {
+    // lunch schema
+    subSchema.lunch = {};
+    subSchema.lunch.address = {};
+    subSchema.lunch.address.coordinates = {};
+    subSchema.lunch.status = {};
+
+    //user sector
+    let userSector = value.lunch.address.area;
+
+    let kitchenManagerDocRef = await db
+      .collection("kitchen")
+      .where(`areaHandling.${userSector}`, "==", true)
+      .get();
+
+    kitchenManagerDocRef.forEach(doc => {
+      console.log("kitchen lunch getting sector", doc.data());
+      let id = doc.id;
+      let kitchenName = doc.data().name;
+
+      subSchema.lunch.kitchen = {};
+      subSchema.lunch.kitchen.id = id;
+      subSchema.lunch.kitchen.name = kitchenName;
+    });
+
+    if (value.lunch.lite) subSchema.lunch.lite = true;
+    if (value.lunch.full) subSchema.lunch.full = true;
+    subSchema.lunch.address.id = value.lunch.address.id;
+    subSchema.lunch.address.tag = value.lunch.address.tag;
+    subSchema.lunch.address.coordinates.latitude =
+      value.lunch.address.coordinates.latitude;
+    subSchema.lunch.address.coordinates.longitude =
+      value.lunch.address.coordinates.longitude;
+    subSchema.lunch.address.address1 = value.lunch.address.address1;
+    subSchema.lunch.address.address2 = value.lunch.address.address2;
+    subSchema.lunch.address.area = value.lunch.address.area;
+    subSchema.lunch.address.city = value.lunch.address.city;
+    subSchema.lunch.price = value.lunch.price;
+    subSchema.lunch.quantity = value.lunch.quantity;
+    if (subSchema.lunch.quantity !== 0) {
+      subSchema.lunch.status.upcoming = true;
+    } else {
+      subSchema.lunch.status.upcoming = false;
+    }
+  }
+
+  if (value.dinner) {
+    //dinner schema
+    subSchema.dinner = {};
+    subSchema.dinner.address = {};
+    subSchema.dinner.address.coordinates = {};
+    subSchema.dinner.status = {};
+
+    //user sector
+    let userSector = value.dinner.address.area;
+
+    let kitchenManagerDocRef = await db
+      .collection("kitchen")
+      .where(`areaHandling.${userSector}`, "==", true)
+      .get();
+
+    kitchenManagerDocRef.forEach(doc => {
+      console.log("kitchen dinner getting sector", doc.data());
+      let id = doc.id;
+      let kitchenName = doc.data().name;
+
+      subSchema.dinner.kitchen = {};
+      subSchema.dinner.kitchen.id = id;
+      subSchema.dinner.kitchen.name = kitchenName;
+    });
+
+    if (value.dinner.lite) subSchema.dinner.lite = true;
+    if (value.dinner.full) subSchema.dinner.full = true;
+    subSchema.dinner.address.id = value.dinner.address.id;
+    subSchema.dinner.address.tag = value.dinner.address.tag;
+    subSchema.dinner.address.coordinates.latitude =
+      value.dinner.address.coordinates.latitude;
+    subSchema.dinner.address.coordinates.longitude =
+      value.dinner.address.coordinates.longitude;
+    subSchema.dinner.address.address1 = value.dinner.address.address1;
+    subSchema.dinner.address.address2 = value.dinner.address.address2;
+    subSchema.dinner.address.area = value.dinner.address.area;
+    subSchema.dinner.address.city = value.dinner.address.city;
+    subSchema.dinner.price = value.dinner.price;
+    subSchema.dinner.quantity = value.dinner.quantity;
+    if (subSchema.dinner.quantity !== 0) {
+      subSchema.dinner.status.upcoming = true;
+    } else {
+      subSchema.dinner.status.upcoming = false;
+    }
+  }
+
+  return subSchema 
+}
+
+function getCalendar(value) {
+  var calenderSchema = {};
+
+  //breakfast
+
+  if (value.breakfast) {
+    calenderSchema.breakfast = {};
+    calenderSchema.breakfast.status = {};
+
+    let address = "";
+    address += `${value.breakfast.address.address1}  `;
+    address += `,${value.breakfast.address.address2} `;
+    address += `,${value.breakfast.address.area}`;
+    address += `,${value.breakfast.address.city}`;
+
+    calenderSchema.breakfast.fullAddress = address;
+    if (value.breakfast.quantity !== 0)
+      calenderSchema.breakfast.status.upcoming = true;
+    else calenderSchema.breakfast.status.upcoming = false;
+
+    calenderSchema.breakfast.price = value.breakfast.price;
+
+    if (value.breakfast.lite) calenderSchema.breakfast.lite = true;
+    if (value.breakfast.full) calenderSchema.breakfast.full = true;
+  }
+  //lunch
+
+  if (value.lunch) {
+    calenderSchema.lunch = {};
+    calenderSchema.lunch.status = {};
+
+    let address = "";
+    address += `${value.lunch.address.address1}  `;
+    address += `,${value.lunch.address.address2} `;
+    address += `,${value.lunch.address.area}`;
+    address += `,${value.lunch.address.city}`;
+
+    calenderSchema.lunch.fullAddress = address;
+    if (value.lunch.quantity !== 0) calenderSchema.lunch.status.upcoming = true;
+    else calenderSchema.lunch.status.upcoming = false;
+
+    calenderSchema.lunch.price = value.lunch.price;
+
+    if (value.lunch.lite) calenderSchema.lunch.lite = true;
+    if (value.lunch.full) calenderSchema.lunch.full = true;
+  }
+
+  // dinner
+  if (value.dinner) {
+    calenderSchema.dinner = {};
+    calenderSchema.dinner.status = {};
+
+    let address = "";
+    address += `${value.dinner.address.address1}  `;
+    address += `,${value.dinner.address.address2} `;
+    address += `,${value.dinner.address.area}`;
+    address += `,${value.dinner.address.city}`;
+
+    calenderSchema.dinner.fullAddress = address;
+    if (value.dinner.quantity !== 0)
+      calenderSchema.dinner.status.upcoming = true;
+    else calenderSchema.dinner.status.upcoming = false;
+
+    calenderSchema.dinner.price = value.dinner.price;
+
+    if (value.dinner.lite) calenderSchema.dinner.lite = true;
+    if (value.dinner.full) calenderSchema.dinner.full = true;
+  }
+
+  return calenderSchema;
+}
+
+function getOrder(value) {
+  var orderSchema = {};
+
+  if (value.breakfast) {
+    //breakfast
+    orderSchema.breakfast = {};
+    orderSchema.breakfast.address = {};
+    orderSchema.breakfast.address.coordinates = {};
+    orderSchema.breakfast.status = {};
+
+    orderSchema.breakfast.address.tag = value.breakfast.address.tag;
+    orderSchema.breakfast.address.coordinates.latitude =
+      value.breakfast.address.coordinates.latitude;
+    orderSchema.breakfast.address.coordinates.longitude =
+      value.breakfast.address.coordinates.longitude;
+    orderSchema.breakfast.address.address1 = value.breakfast.address.address1;
+    orderSchema.breakfast.address.address2 = value.breakfast.address.address2;
+    orderSchema.breakfast.address.area = value.breakfast.address.area;
+    orderSchema.breakfast.address.city = value.breakfast.address.city;
+
+    if (value.breakfast.quantity !== 0)
+      orderSchema.breakfast.status.upcoming = true;
+    else orderSchema.breakfast.status.upcoming = false;
+
+    orderSchema.breakfast.price = value.breakfast.price;
+    if (value.breakfast.lite) orderSchema.breakfast.lite = true;
+    if (value.breakfast.full) orderSchema.breakfast.full = true;
+  }
+  if (value.lunch) {
+    //lunch
+    orderSchema.lunch = {};
+    orderSchema.lunch.address = {};
+    orderSchema.lunch.address.coordinates = {};
+    orderSchema.lunch.status = {};
+
+    orderSchema.lunch.address.tag = value.lunch.address.tag;
+    orderSchema.lunch.address.coordinates.latitude =
+      value.lunch.address.coordinates.latitude;
+    orderSchema.lunch.address.coordinates.longitude =
+      value.lunch.address.coordinates.longitude;
+    orderSchema.lunch.address.address1 = value.lunch.address.address1;
+    orderSchema.lunch.address.address2 = value.lunch.address.address2;
+    orderSchema.lunch.address.area = value.lunch.address.area;
+    orderSchema.lunch.address.city = value.lunch.address.city;
+
+    if (value.lunch.quantity !== 0) orderSchema.lunch.status.upcoming = true;
+    else orderSchema.lunch.status.upcoming = false;
+
+    orderSchema.lunch.price = value.lunch.price;
+    if (value.lunch.lite) orderSchema.lunch.lite = true;
+    if (value.lunch.full) orderSchema.lunch.full = true;
+  }
+  if (value.dinner) {
+    //dinner
+    orderSchema.dinner = {};
+    orderSchema.dinner.address = {};
+    orderSchema.dinner.address.coordinates = {};
+    orderSchema.dinner.status = {};
+
+    orderSchema.dinner.address.tag = value.dinner.address.tag;
+    orderSchema.dinner.address.coordinates.latitude =
+      value.dinner.address.coordinates.latitude;
+    orderSchema.dinner.address.coordinates.longitude =
+      value.dinner.address.coordinates.longitude;
+    orderSchema.dinner.address.address1 = value.dinner.address.address1;
+    orderSchema.dinner.address.address2 = value.dinner.address.address2;
+    orderSchema.dinner.address.area = value.dinner.address.area;
+    orderSchema.dinner.address.city = value.dinner.address.city;
+
+    if (value.dinner.quantity !== 0) orderSchema.dinner.status.upcoming = true;
+    else orderSchema.dinner.status.upcoming = false;
+
+    orderSchema.dinner.price = value.dinner.price;
+    if (value.dinner.lite) orderSchema.dinner.lite = true;
+    if (value.dinner.full) orderSchema.dinner.full = true;
+  }
+
+  return orderSchema;
+}
+
 route.post("/", (req, res) => {
   const schema = {};
 
   let { error, value } = Joi.validate(req.body.users, schema);
 
+  error = false;
   if (error) {
-    console.log("Post trial schema error");
+    console.log("Post trial schema error",error.details[0].message);
     return res.status(400).json({
       error: {
         message: `Post trial schema error , ${error.details[0].message}`
       }
     });
   } else {
-    let date = req.body.users.date;
+
+    let batch = db.batch()
+
+    let date = req.body.date;
 
     date = new Date(date);
 
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
+    let date = date.getDate()
+
+    // for proper formatting
+    if (date < 10) {
+      date = "0" + date;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    //// user subscriptions ////
+    console.log('user trial subscription start')
+    let SubscriptionData = await getSubscriptions(req.body.users.trial)
+
+    let userSubscriptionsRef = await db.collection('users').doc(req.body.users.id).collection('subscriptions')
+
+    let docId = `${date}${month}${year}`
+    let userSubDocRef = userSubscriptionsRef.doc(docId)
+
+    batch.set(userSubDocRef, SubscriptionData,{merge : true})
+    console.log('user trial subscription ended')
+
+    /////  completed user subscription //////////
+
+    /////  user calendar starting ///////////////
+
+    console.log('user trial calendar start')
+
+    let calendarData = await getCalendar(req.body.users.trial)
+
+    let userCalendarDocRef = db.collection('users').doc(req.body.users.id).collection('calendar').doc(`${month}${year}`)
+
+    batch.set(userCalendarDocRef, {[date] : calendarData}, {merge : true})
+    
+    console.log('user trial calendar ended')
+
+    //// user calendar ended ////////
+
+    ///// order started //// 
+    console.log('order batch started')
+
+    let orderData = getOrder(req.body.users.trial)
+
+    let orderDocRef = db.collection('orders').doc(`${month}${year}`).collection(`${date}${month}${year}`).doc(req.body.users.id)
+
+    batch.set(orderDocRef, orderData, {merge : true})
+
+    console.log('order batch ended ')
+    //// order ended ///////
+
+
+    //// kitchen starting ////////
+    console.log('kitchen batch starting')
+
+
+
+    console.log('kitchen batch ended')
+    
+    /// kitchen ended //////////////
+    console.log('starting batch of kitchen collection')
+    
+    //breakfast
+    if(req.body.users.trial.break){
+      let userSector = req.body.users.trial.breakfast.address.area 
+
+      let kitchenManagerDocRed = await db.collection('kitchens').where()
+    }
+    console.log('ended batch of kitchen collection')
+
+
+
   }
 });
 
